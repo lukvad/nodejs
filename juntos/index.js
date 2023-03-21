29afac416e4e1b1f52e8d0605338531aabe08634
lukvad <35584815+lukvad@users.noreply.github.com>
2023-03-21 13:35:29 +0100

    Finished

--- juntos/index.js
+++ juntos/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const methodOverride = require('method-override');
 const app = express()
 const mongoose = require('mongoose');
 const path = require('path');
 const Product = require('./models/products')
+const categories = ['fruit', 'vegetables', 'dairy', 'fungi']
 
 mongoose.connect('mongodb://localhost:27017/farmStand')
     .then(() => {
@@ -14,7 +16,9 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
     })
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
@@ -22,7 +26,25 @@ app.get('/products', async (req, res) => {
 })
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new')
+    res.render('products/new', { categories })
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findById(id)
+    res.render('products/edit', { product, categories })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+    res.redirect(`/products/${id}`)
+})
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params
+    await Product.findByIdAndDelete(id)
+    res.redirect('/products')
 })
 
 app.get('/products/:id', async (req, res) => {
