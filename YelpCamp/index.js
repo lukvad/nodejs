3b2d14df057a8530db9dae06bba8ea6490e6acd8
lukvad <35584815+lukvad@users.noreply.github.com>
2023-03-23 20:46:20 +0100

    Morgan

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground')
+const method = require('method-override');
+const morgan = require('morgan');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -15,6 +17,9 @@ db.once('open', () => {
 })
 
 const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(method('_method'))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -23,10 +28,43 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
+app.use(morgan('dev'))
+
+
+app.get('/dogs', (req, res) => {
+    res.send("WOOF WOOF")
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findById(id)
+    res.render('campgrounds/edit', { campground })
+})
 app.get('/campgrounds/:id', async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findById(id)
     res.render('campgrounds/show', { campground })
+})
+
+app.put("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.render('campgrounds/show', { campground })
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
 })
 
 app.listen(3000, () => {
