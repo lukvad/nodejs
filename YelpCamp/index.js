a076a70cb831b8badb88d5caba3d09b45f533738
lukvad <35584815+lukvad@users.noreply.github.com>
2023-03-30 20:16:39 +0200

    Error templates

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -5,7 +5,8 @@ const Campground = require('./models/campground')
 const method = require('method-override');
 const morgan = require('morgan');
 const ejsMate = require('ejs-mate');
-const AppError = require('./AppError');
+const expressError = require('./utils/ExpressError');
+const catchAsync = require('./utils/catchAsync');
 const { wrap } = require('module');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -29,13 +30,8 @@ app.engine('ejs', ejsMate)
 
 app.use(morgan('tiny'))
 
-function wrapAsync(fn) {
-    return function (req, res, next) {
-        fn(req, res, next).catch(e => next(e))
-    }
-}
 
-app.get('/campgrounds', wrapAsync(async (req, res, next) => {
+app.get('/campgrounds', catchAsync(async (req, res, next) => {
     const campgrounds = await Campground.find({})
     res.render('campgrounds/index', { campgrounds })
 }))
@@ -44,91 +40,64 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
 
-app.get('/campgrounds/:id/edit', wrapAsync(async (req, res, next) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res, next) => {
     const { id } = req.params
     const campground = await Campground.findById(id)
     if (!campground) {
-        return next(new AppError('Product not found', 404))
+        return next(new expressError('Product not found', 404))
     }
     res.render('campgrounds/edit', { campground })
 }))
 
 
-app.get('/campgrounds/:id', wrapAsync(async (req, res, next) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res, next) => {
 
     const { id } = req.params
     const campground = await Campground.findById(id)
     if (!campground) {
-        throw new AppError('Product not found', 404)
+        throw new expressError('Product not found', 404)
     }
     res.render('campgrounds/show', { campground })
 }))
 
-app.put("/campgrounds/:id", wrapAsync(async (req, res, next) => {
+app.put("/campgrounds/:id", catchAsync(async (req, res, next) => {
+    if (!req.body.campground) {
+        throw new expressError('Invalid Data Posted', 400)
+    }
     const { id } = req.params
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { runValidators: true, new: true })
     if (!campground) {
-        throw new AppError('No such Id', 404)
+        throw new expressError('No such Id', 404)
     }
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-app.post('/campgrounds', wrapAsync(async (req, res, next) => {
-    const campground = new Campground(req.body.campground)
-    if (!campground) {
-        throw new AppError('no campground posted', 404)
+app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    if (!req.body.campground) {
+        throw new expressError('no campground posted', 400)
     }
+    const campground = new Campground(req.body.campground)
     await campground.save()
     res.render('campgrounds/show', { campground })
 }))
 
-app.delete('/campgrounds/:id', wrapAsync(async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findByIdAndDelete(id)
     if (!campground) {
-        throw new AppError('No such Id', 404)
+        throw new expressError('No such Id', 404)
     }
     res.redirect('/campgrounds')
 }))
 
-
-
-
-app.get('/error', (req, res) => {
-    chicken.fly()
-})
-
-const verifyPassword = (req, res, next) => {
-    const { password } = req.query
-    if (password === 'chickennuggets') {
-        return next()
-    }
-    throw new AppError('Password needed!', 401)
-}
-app.get('/secret', verifyPassword, (req, res) => {
-    res.send('My secret is : bla bla')
-})
-
-app.get('/admin', (req, res) => {
-    throw new AppError('You are not an admin!', 403)
-})
-
-const handleValError = err => {
-    console.dir(err);
-    return err
-}
-
-app.use((err, req, res, next) => {
-    if (err.name === 'ValidationError') err = handleValError(err)
-    next(err)
+app.all('*', (req, res, next) => {
+    next(new expressError('Page Not Found', 404))
 })
 
 app.use((err, req, res, next) => {
-    const { status = 500, message = 'Something went wrong' } = err
-    res.status(status).send(message)
+    const { statusCode = 500, message = 'Something Went Wrong' } = err
+    res.status(statusCode).send(message)
 })
-
-
 
 app.listen(3000, () => {
     console.log("Listening to port 3000");
